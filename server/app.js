e078e0569aac95b27e03951d49b4d4ae6a71f2a2
dengke0427@gmail.com <dengke0427@gmail.com>
2020-04-12 15:16:57 -0700

    CD-200412: Added routes.js to the project

--- server/app.js
+++ server/app.js
@@ -1,14 +1,7 @@
 const http = require('http');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>CountUp first Page</title></head>');
-    res.write('<body><h1>Hello from Node.js</h1><body>');
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(routes);
 
 // we listen on port 3000
 server.listen(3000);
---
+++ server/routes.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>CountUp first Page</title></head>');
+        res.write('<body><h1><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('./hello.txt', message);
+            return res.end();
+        });
+    }
+    res.end();
+}  
+
+module.exports = requestHandler;
